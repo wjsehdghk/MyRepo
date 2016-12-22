527de7b681c245261222722a0776c8ccbe555bd8
wjsehdghk <wjsehdghk91@naver.com>
2016-12-22 18:13:44 +0900

    videoview added

--- surfaceVideoTest/app/src/main/java/com/example/administrator/surface/Adapter/VideoAdapter.java
+++ surfaceVideoTest/app/src/main/java/com/example/administrator/surface/Adapter/VideoAdapter.java
@@ -9,42 +9,32 @@ import android.widget.ImageView;
 
 import com.example.administrator.surface.Model.VideoList;
 import com.example.administrator.surface.R;
+import com.h6ah4i.android.widget.advrecyclerview.draggable.DraggableItemAdapter;
+import com.h6ah4i.android.widget.advrecyclerview.draggable.ItemDraggableRange;
+import com.h6ah4i.android.widget.advrecyclerview.utils.AbstractDraggableItemViewHolder;
 
 import java.util.List;
 
 /**
  * Created by Administrator on 2016-09-13.
  */
-public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.MyViewHolder> {
-
-
+public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.MyViewHolder> implements DraggableItemAdapter<VideoAdapter.MyViewHolder> {
     List<VideoList> vList;
     Context context;
-
     public VideoAdapter(List<VideoList> vList, Context context) {
+        setHasStableIds(true);
         this.vList = vList;
         this.context = context;
     }
-
-
     private static OnitemClickListener listener;
-
     public interface OnitemClickListener{
         void onItemClick(View itemview, int position);
     }
-
-
-
     public void setOnItemClickListener(OnitemClickListener listener){
         this.listener=listener;
     }
-
-
-    static class MyViewHolder extends RecyclerView.ViewHolder{
-
-
+    public class MyViewHolder extends AbstractDraggableItemViewHolder{
         ImageView imageView;
-
         public MyViewHolder(final View itemView){
             super(itemView);
             imageView= (ImageView)itemView.findViewById(R.id.Thumbnail);
@@ -57,20 +47,37 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.MyViewHolder
             });
         }
     }
-
+    @Override
+    public boolean onCheckCanStartDrag(MyViewHolder holder, int position, int x, int y) {
+        return true;
+    }
+    @Override
+    public ItemDraggableRange onGetItemDraggableRange(MyViewHolder holder, int position) {
+        return null;
+    }
+    @Override
+    public void onMoveItem(int fromPosition, int toPosition) {
+        VideoList videoList= vList.remove(fromPosition);
+        vList.add(toPosition,videoList);
+        notifyItemMoved(fromPosition,toPosition);
+    }
+    @Override
+    public boolean onCheckCanDrop(int draggingPosition, int dropPosition) {
+        return true;
+    }
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
       View itemview = LayoutInflater.from(parent.getContext()).inflate(R.layout.videolist,parent,false);
         return new MyViewHolder(itemview);
     }
-
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-
         VideoList videoList = vList.get(position);
         holder.imageView.setImageBitmap(videoList.getThumbImage());
-
-
+    }
+    @Override
+    public long getItemId(int position) {
+        return vList.get(position).id;
     }
 
     @Override
--- surfaceVideoTest/app/src/main/java/com/example/administrator/surface/MainActivity.java
+++ surfaceVideoTest/app/src/main/java/com/example/administrator/surface/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.administrator.surface;
 
-import android.app.ProgressDialog;
-import android.content.Context;
 import android.graphics.Bitmap;
-import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -11,98 +8,87 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.MediaController;
-import android.widget.VideoView;
-
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 import com.example.administrator.surface.Adapter.VideoAdapter;
 import com.example.administrator.surface.Model.VideoList;
-
+import com.h6ah4i.android.widget.advrecyclerview.draggable.RecyclerViewDragDropManager;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-
 import wseemann.media.FFmpegMediaMetadataRetriever;
 
-public class MainActivity extends AppCompatActivity {
 
+
+
+public class MainActivity extends AppCompatActivity {
     public final static String VIDEO_URL = "http://techslides.com/demos/sample-videos/small.mp4";
-    public final static String VIDEO_URL2 = "http://www.androidbegin.com/tutorial/AndroidCommercial.3gp";
-    RecyclerView recyclerView;
-    VideoView videoView;
-    List<VideoList> videoList = new ArrayList<>();
+    public final static String VIDEO_URL2 = "http://www.html5videoplayer.net/videos/toystory.mp4";
+    public final static String VIDEO_URL3 = "http://www.archive.org/download/Unexpect2001/Unexpect2001_512kb.mp4";
+    public List<VideoList> videoList = new ArrayList<>();
     VideoAdapter videoAdapter;
+    CustomView customView;
     FFmpegMediaMetadataRetriever mmr;
-    ProgressDialog pDialog;
+    RecyclerView recyclerView;
+    RecyclerViewDragDropManager dragmgr;
+    FrameLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         recyclerView = (RecyclerView) findViewById(R.id.recycler);
-        videoView = (VideoView) findViewById(R.id.videoView);
-        pDialog = new ProgressDialog(getBaseContext());
-        pDialog.setTitle("동영상 로딩중");
-        pDialog.setMessage("Buffering");
-        pDialog.setCancelable(false);
-        pDialog.setIndeterminate(false);
+        container = (FrameLayout) findViewById(R.id.container);
         Thumbload();
-
-        //
-
+        customView = new CustomView(getBaseContext());
+        customView.setOnTouchListener(customView);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(200, 200);
+        container.addView(customView);
+        customView.requestFocus();
+        dragmgr = new RecyclerViewDragDropManager();
+        dragmgr.setInitiateOnMove(false);
+        dragmgr.setInitiateOnLongPress(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getBaseContext(), LinearLayoutManager.HORIZONTAL, false));
         videoAdapter = new VideoAdapter(videoList, getBaseContext());
-        recyclerView.setAdapter(videoAdapter);
-
-        videoAdapter.setOnItemClickListener(new VideoAdapter.OnitemClickListener() {
+        recyclerView.setAdapter(dragmgr.createWrappedAdapter(videoAdapter));
+        dragmgr.attachRecyclerView(recyclerView);}
+        /*videoAdapter.setOnItemClickListener(new VideoAdapter.OnitemClickListener() {
             @Override
             public void onItemClick(View itemview, int position) {
-
-
-                //pDialog.show();
                 try {
-                    videoView.setVideoURI(Uri.parse(VIDEO_URL));
+                   // customView.videoView.setVideoURI(Uri.parse(videoList.get(position).getURL()));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-
-                videoView.requestFocus();
-                videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                customView.requestFocus();
+              //  customView.videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
                     @Override
                     public void onPrepared(MediaPlayer mediaPlayer) {
-
-                        //pDialog.dismiss();
-                        videoView.start();
+                       // customView.videoView.start();
                     }
                 });
             }
         });
-
     }
-
-
+*/
     public void Thumbload() {
-
         mmr = new FFmpegMediaMetadataRetriever();
         mmr.setDataSource(VIDEO_URL);
         mmr.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_ALBUM);
         mmr.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_ALBUM_ARTIST);
-        Bitmap bitmap = mmr.getFrameAtTime(-1, FFmpegMediaMetadataRetriever.OPTION_CLOSEST); //썸네일
-
-
+        Bitmap bitmap = mmr.getFrameAtTime(2000000, FFmpegMediaMetadataRetriever.OPTION_CLOSEST);
+        mmr.setDataSource(VIDEO_URL3);
+        mmr.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_ALBUM);
+        mmr.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_ALBUM_ARTIST);
+        Bitmap bitmap1 = mmr.getFrameAtTime(2000000, FFmpegMediaMetadataRetriever.OPTION_CLOSEST);
         mmr.setDataSource(VIDEO_URL2);
         mmr.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_ALBUM);
         mmr.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_ALBUM_ARTIST);
-        Bitmap bitmap2 = mmr.getFrameAtTime(-1, FFmpegMediaMetadataRetriever.OPTION_CLOSEST);
-
-
-        // mmr.release();
-
-        VideoList v = new VideoList(bitmap);
+        Bitmap bitmap2 = mmr.getFrameAtTime(2000000, FFmpegMediaMetadataRetriever.OPTION_CLOSEST);
+        VideoList v = new VideoList(bitmap, VIDEO_URL, 1);
         videoList.add(v);
-
-        v = new VideoList(bitmap2);
+        v = new VideoList(bitmap2, VIDEO_URL2, 2);
         videoList.add(v);
-
+        v = new VideoList(bitmap1, VIDEO_URL3, 3);
+        videoList.add(v);
     }
 }
